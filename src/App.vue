216e7dbb49c2aee9ba11699a4a64a899d2fa3442
Diego Bonilla <diego.bonilla@peaku.io>
2023-06-12 09:49:01 -0500

    some styles

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
-  <div>
-    <h1>Autocomplete Input</h1>
+  <div class="ppal">
+    <h1 class="title">Autocomplete email manager</h1>
     <AutocompleteInput></AutocompleteInput>
   </div>
 </template>
@@ -14,3 +14,15 @@ export default {
   },
 };
 </script>
+<style>
+ .title {
+  color: blue;
+  text-align: center;
+  font-size: 28px;
+  font-weight: bold;
+  margin: 0;
+  padding: 0;
+  border: 0;
+  font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+ }
+</style>
